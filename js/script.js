77c1a9e313c4ff3555d980b82e68367a1a1a871f
Touhid Khan <touhidkhan86844@gmail.com>
2026-03-04 01:54:08 +0600

    Added responsive navbar and mobile hamburger toggle functionality with CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("The Golden Spoon Loaded");
+// Hamburger menu toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
